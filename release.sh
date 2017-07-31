a09841a9c3a6da03e74ff6d3853d954be4b0fa03
Romain Grecourt <romain.grecourt@oracle.com>
2017-07-31 11:14:20 -0700

    update pom and release.sh to release from Github

--- release.sh
+++ release.sh
@@ -10,9 +10,21 @@
 #            <password>password</password>
 #        </server>
 #    </servers>
+#    <profiles>
+#      <profile>
+#        <id>release</id>
+#        <properties>
+#          <user.name>jvnet_id</user.name>
+#          <release.arguments>-Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dgpg.passphrase=glassfish</release.arguments>
+#        </properties>
+#        <activation>
+#          <activeByDefault>false</activeByDefault>
+#        </activation>
+#      </profile>
+#    </profiles>
 
 # see the following URL for gpg issues
 # https://docs.sonatype.org/display/Repository/How+To+Generate+PGP+Signatures+With+Maven#HowToGeneratePGPSignaturesWithMaven-GenerateaKeyPair
 
-mvn -B release:prepare -DpreparationGoals="install -P\!jvnet-release"
-mvn -B release:perform -Dgoals="deploy -Dhttps.proxyHost=inet-rmmc01.oracle.com -Dhttps.proxyPort=80 -Dgpg.passphrase=glassfish"
+mvn -B -e release:prepare -Prelease
+mvn -B -e release:perform -Prelease
